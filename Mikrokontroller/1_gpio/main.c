d09271798fc0a06128d5ed7f49efa2d70eda0fc5
frafjord1 <andrfra@stud.ntnu.no>
2025-04-01 14:38:50 +0200

    fiksa oppgave 1

--- Mikrokontroller/1_gpio/main.c
+++ Mikrokontroller/1_gpio/main.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+#define GPIO ((NRF_GPIO_REGS*)0x50000000)
 
 typedef struct {
 	volatile uint32_t RESERVED0[321];
@@ -13,12 +13,13 @@ typedef struct {
 	volatile uint32_t DIRCLR;
 	volatile uint32_t LATCH;
 	volatile uint32_t DETECTMODE;
-	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
+	volatile uint32_t RESERVED1[0x76];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
 void button_init(){ 
-	GPIO->PIN_CNF[__BUTTON_1_PIN__] = (3 << 2);
+	GPIO->PIN_CNF[0x734] = (0 << 0) | (3 << 2);
+	GPIO->PIN_CNF[0x738] = (0 << 0) | (3 << 2);
 	// Fill inn the configuration for the remaining buttons 
 }
 
@@ -31,17 +32,33 @@ int main(){
 
 	// Configure buttons -> see button_init()
 
+
+	button_init();
+
+
 	int sleep = 0;
 	while(1){
-
-		/* Check if button 1 is pressed;
-		 * turn on LED matrix if it is. */
-
-		/* Check if button 2 is pressed;
-		 * turn off LED matrix if it is. */
+    while (1) {
+        if ((GPIO->IN & (1 << 13)) == 0) { 
+            for (int i = 17; i <= 20; i++) {
+                GPIO->OUTSET = (1 << i);
+            }
+        } 
+        else if ((GPIO->IN & (1 << 14)) == 0) { 
+            for (int i = 17; i <= 20; i++) {
+                GPIO->OUTCLR = (1 << i);
+            }
+        }
 
 		sleep = 10000;
 		while(--sleep); // Delay
 	}
 	return 0;
 }
+
+
+//oppgave 1.2: pinnene SW1 (button 1) og SW2 (button 2) er henholdsvis tilkoblet pin Po.13 og Po.14. 
+//fra kretsskjemaet ser vi at disse pinnene er logisk høye så lenge de ikke er trykket ned.
+//Når de trykkes ned, blir de koblet til jord og dermed logisk lave. 
+
+//oppgave 1.3: Baseadressen til GPIO-modulen er 0x50000000.
