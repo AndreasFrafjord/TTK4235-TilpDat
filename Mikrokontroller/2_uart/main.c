ecfb38d1d2fc62fcafa05cf62bc24561a24eea71
frafjord1 <andrfra@stud.ntnu.no>
2025-04-03 13:14:23 +0200

    fiksa på main.c

--- Mikrokontroller/2_uart/main.c
+++ Mikrokontroller/2_uart/main.c
@@ -6,12 +6,55 @@
 
 
 int main(){
-	// Configure LED Matrix
+    // Configure LED Matrix
 	for(int i = 17; i <= 20; i++){
 		GPIO->DIRSET = (1 << i);
-		GPIO->OUTCLR = (1 << i);
+		GPIO->OUTCLR = (0 << i);  //dette kan hende ikke fungerer
 	}
 
+	GPIO->PIN_CNF[13] = (0 << 0) | (3 << 2);
+	GPIO->PIN_CNF[14] = (0 << 0) | (3 << 2);
+	// Fill inn the configuration for the remaining buttons 
+
+
+
+    uart_init();
+    char letter = 0;
+    int led_on = 0;
+
+    while (1) {
+        if ((GPIO->IN & (1 << 13)) == 0) { 
+            uart_send("A");
+        } 
+        else if ((GPIO->IN & (1 << 14)) == 0) {
+            uart_send("B");
+        }
+
+
+		sleep = 10000;
+		while(--sleep); // Delay
+    }
+
+    if (UART->EVENTS_RXDRDY) { 
+        letter = uart_read(); 
+        UART->EVENTS_RXDRDY = 0; 
+
+        
+        if (led_on) {
+            for (int i = 17; i <= 20; i++) {
+                GPIO->OUTSET = (1 << i);
+            }
+            led_on = 0;
+        } else {
+            for (int i = 17; i <= 20; i++) {
+                GPIO->OUTCLR = (1 << i);
+            }
+            led_on = 1;
+        }
+    }
+    
+    return 0;
+}
 
 
 
